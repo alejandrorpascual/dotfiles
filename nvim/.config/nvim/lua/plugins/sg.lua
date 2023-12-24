0aedf0785c08acbd975c4f630f72f0dd10028e6d
Alejandro R Pascual <alejandrorpascual@Alejandros-MacBook-Air.local>
2023-12-24 18:04:16 -0400

    wip

--- nvim/.config/nvim/lua/plugins/sg.lua
+++ nvim/.config/nvim/lua/plugins/sg.lua
@@ -1,26 +1,13 @@
 return {
     "sourcegraph/sg.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-        {
-            "<space>ct",
-            "<cmd>CodyToggle<CR>",
-            "Toggle Cody",
-        },
-        {
-            "<space>ch",
-            "<cmd>CodyChat<CR>",
-            "Cody Chat",
-        },
-        {
-            "<space>ss",
-            "<cmd>lua require('sg.extensions.telescope').fuzzy_search_results()<CR>",
-            "Search Sourcegraph",
-        },
-    },
     build = "nvim -l build/init.lua",
     config = function()
         require('sg').setup({})
+        bind("n", "<space>ss", "<cmd>lua require('sg.extensions.telescope').fuzzy_search_results()<CR>",
+            "Search Sourcegraph")
+        bind("n", "<space>ct", "<cmd>CodyToggle<CR>", "Toggle Cody")
+        bind("n", "<space>ch", "<cmd>CodyChat<CR>", "Cody Chat")
     end,
 
 }
