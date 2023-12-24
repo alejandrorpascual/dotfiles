8a4c7a3c2ed0d373a443b968cf59ebeb5931bf58
Alejandro R Pascual <alejandrorpascual@Alejandros-MacBook-Pro.local>
2023-12-24 12:12:16 -0400

    wip

--- nvim/.config/nvim/lua/config/refactoring.lua
+++ nvim/.config/nvim/lua/config/refactoring.lua
@@ -1,37 +1,15 @@
 local refactoring = require("refactoring")
 refactoring.setup({})
 
-bind("v", "<leader>re", function()
-    refactoring.refactor("Extract Function")
-end, "Refactoring: Extract Function")
-
-bind("v", "<leader>rf", function()
-    refactoring.refactor("Extract Function To File")
-end, "Refactoring: Extract Function To File")
-
-bind("v", "<leader>rv", function()
-    refactoring.refactor("Extract Variable")
-end, "Refactoring: Extract Variable")
-
-bind("v", "<leader>ri", function()
-    refactoring.refactor("Inline Variable")
-end, "Refactoring: Inline Variable")
-
+bind("v", "<leader>re", ":Refactor extract<cr>", "Refactoring: Extract Function")
+bind("v", "<leader>rf", ":Refactor extract_to_file<cr>", "Refactoring: Extract Function To File")
+bind("v", "<leader>rv", ":Refactor extract_var<cr>", "Refactoring: Extract Variable")
+bind("x", "<leader>ri", ":Refactor inline_var<cr>", "Refactoring: Inline Variable")
+bind("n", "<leader>ri", ":Refactor inline_var<cr>", "Refactoring: Inline Variable")
 -- Extract block doesn't need visual mode
-bind("n", "<leader>rb", function()
-    refactoring.refactor("Extract Block")
-end, "Refactoring: Extract Block")
-
-bind("n", "<leader>rbf", function()
-    refactoring.refactor("Extract Block To File")
-end, "Refactoring: Extract Block To File")
-
--- Inline variable can also pick up the identifier currently under the cursor without visual mode
-bind("n", "<leader>ri", function()
-    refactoring.refactor("Inline Variable")
-end, "Refactoring: Inline Variable")
-
+bind("n", "<leader>rb", ":Refactor extract_block<cr>", "Refactoring: Extract Block")
+bind("n", "<leader>rbf", ":Refactor extract_block_to_file", "Refactoring: Extract Block To File")
 -- prompt for a refactor to apply when the remap is triggered
 bind("v", "<leader>rr", function()
-    refactoring.prompt_refactor()
+    require('refactoring').select_refactor({})
 end, "Refactoring: Prompt Refactor")
